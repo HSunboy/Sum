b8235c7c943cc5d1bb61ed279c2165a437fbbc97
softthree-4 <softthree-4@softthree-4tekiimac.local>
2015-10-13 15:48:42 +0800

    Initial Commit

---
+++ Sum/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sum
+//
+//  Created by softthree-4 on 15/10/13.
+//  Copyright © 2015年 softthree-4. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
